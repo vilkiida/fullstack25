540633e30211b21d9c9c5f1a023ebb08866a0c04
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2025-08-18 20:00:19 +0300

    1.2 done

--- osa1/kurssitiedot/kurrsitiedot/src/App.jsx
+++ osa1/kurssitiedot/kurrsitiedot/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Content part1={part1} ex1={exercises1} part2={part2} ex2={exercises2} part3={part3} ex3={exercises3} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
@@ -22,18 +22,19 @@ const Header = (props) => (
   </>
 )
 
-const Content = (props) => (
-  <>
-    <p>
-      {props.part1} {props.exercises1}   
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
-  </>
+const Content = (props) => {
+  return(
+    <div>
+      <Part part={props.part1} ex={props.ex1} />
+      <Part part={props.part2} ex={props.ex2} />
+      <Part part={props.part3} ex={props.ex3} />
+    </div>
+  )
+}
+
+
+const Part = (props) => (
+  <p>{props.part} {props.ex}</p>
 )
 
 const Total = (props) => (
